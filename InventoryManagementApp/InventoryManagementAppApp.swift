194f86b8f1594f102a3f2c1660faabc1782f0ab0
Hercio Venceslau Silla <111202113945@mhs.dinus.ac.id>
2024-12-10 10:14:23 +0700

    Initial Commit

---
+++ InventoryManagementApp/InventoryManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManagementAppApp.swift
+//  InventoryManagementApp
+//
+//  Created by Hercio Venceslau Silla on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InventoryManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InventoryManagementAppTests/InventoryManagementAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManagementAppTests.swift
+//  InventoryManagementAppTests
+//
+//  Created by Hercio Venceslau Silla on 10/12/24.
+//
+
+import Testing
+@testable import InventoryManagementApp
+
+struct InventoryManagementAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
